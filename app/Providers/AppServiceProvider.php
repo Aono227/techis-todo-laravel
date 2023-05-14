53f9c4605f02ec0aa338284e41aeefbb1dccdb72
125106702+Aono227@users.noreply.github.com <125106702+Aono227@users.noreply.github.com>
2023-05-14 18:10:01 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if(\app::enviroment(['production']))
+        if(\App::environment(['production'])) {
            \URL::forceScheme('https');
+        }
     }
 }
